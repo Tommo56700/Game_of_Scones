3591cce7148c572f0c22cc22317a0ee581c7d231
SparkeyRob <rdstaylor2@gmail.com>
2016-10-11 16:25:44 +0100

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,22 @@
+"Welcome!"
+"In a world where baking is the only form of solice, one chef strives to please the formidable Mary Berry."
+"You have set yourself on a quest to source the rarest ingrediants in all four kingdoms"
+"In your travels you will have to solve complexing puzzles, battle the most ferocious beasts and bake the tastiest scone the world has ever known."
+"Will you make it through these rigorous challenges?"
+"Find out now in..."
+"Game of Scones!"
+
+"Quest 1: Blood, Sweat and Tears"
+"Find all 4 ingredients to make the ultimate scone"
+
+	"Congratulations, you found the Flour"
+	"Congratulations, you found the Sugar"
+	"Congratulations, you found the Egg"
+	"Congratulations, you found the Milk"
+
+"Quest 2: The Clock is Ticking"
+"Find your way back to Mary Berry before she gets hungry!"
+
+
+"Quest 3: The Final Test"
+"Prove yourself to Merry Berry or die trying"
