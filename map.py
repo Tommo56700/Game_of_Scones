f775550773a5853df6e3f2a50283f4e44be35fca
SparkeyRob <rdstaylor2@gmail.com>
2016-10-17 16:42:53 +0100

    Major Configurations
    Sorry Sean for messing everything up

--- map.py
+++ map.py
@@ -7,7 +7,7 @@ room_centre = {
 
     "name": "Main Baking Room",
 
-    "story": "*** description ***",
+    "story": "You have infiltrated the lair of the queen, but be careful not to wake the beast.\nThe journey here so far has been a long and challenging one, but things are\nabout to get harder. You find yourself standing in a vast, expansive room in\nthe centre of the Red Velvet Keep, the palace from which Mary Berry pulls the\nstrings over the Seven Kingdoms. The room is airily chilling.\nThe air is filled with flour dust, illuminated by strands of light beaming in\nfrom the windows. The walls are adorned with paintings of baked goods and in\nthe centre is a stone statue of a croquembouche.  On the far wall is a large,\narched doorway with two key holes. One appears to be made from ginger bread,\nthe other from shortbread. On one side is a door labelled Pantry. Whilst, on the\nother side is a door labelled Cellar, and a stairway leading down.",
 
     "exits": {"north": "room_puzzle_maze"},
 
@@ -28,132 +28,83 @@ room_puzzle_maze = {
     "event": "maze"
 }
 
-room_puzzle_2 = {
-    "id": "",
+room_puzzle_keypad = {
+    "id": "puzzle_keypad",
 
-    "name": "Sue's Cellar",
+    "name": "Cellar",
 
-    "story": "*** description ***",
+    "story": "You descend into the under belly of Mary’s fortress of pain. The cellar is dark\nand damp, the only light coming from a few flickering candles hung on the walls.\nEmpty crates and wooden pallets scatter the room. You notice on the floor an\nelaborate rug, an heirloom of the Berry family no doubt. One of the corners is\nupturned slightly. On the far wall is a door labelled store room.",
 
     "exits": {},
 
-    "items": []
+    "items": ["***Combat ITEM***"]
 }
 
-room_puzzle_3 = {
-    "id": "",
+room_puzzle_crossword = {
+    "id": "puzzle_crossword",
 
     "name": "Paul's Pantry",
 
-    "story": "*** description ***",
+    "story": "You are in the pantry. Surrounding you from all directions are shelves towering\nall the way to the ceiling, stacked full of pastries, cakes, loafs of bread and\na plethora of cooking ingredients. You notice a chest with the lid slightly\najar. On the far wall is a large freezer door.",
 
     "exits": {},
 
-    "items": []
+    "items": ["***Combat ITEM***"]
 }
 
-room_puzzle_4 = {
-    "id": "",
-
-    "name": "Dough Room",
-
-    "story": "*** description ***",
-
-    "exits": {},
-
-    "items": []
-}
-
-room_puzzle_5 = {
-    "id": "",
-
-    "name": "Proving Room",
-
-    "story": "*** description ***",
-
-    "exits": {},
-
-    "items": []
-}
-
-room_puzzle_trivia = {
-    "id": "",
-
-    "name": "Walk-in Freezer",
-
-    "story": "*** description ***",
-
-    "exits": {},
-
-    "event": "trivia",
-
-    "items": []
-}
 
 room_boss_mary = {
     "id": "room_boss_mary",
 
     "name": "boss_1",
 
-    "story": "*** story ***",
+    "story": "You have made it this far but you must now rely on your baking ability more\nthan ever. This is it. The final showdown. The stage is set, the show must now\nunfold. And the far end of the room is a great fiery furnace, the heat from\nwhich is scolding even from this distance. You have well and truly descended\ninto Mary’s hell. In front of the furnace you see four ingredients laid out on a\ntable; mill, flour, butter and eggs. But there in front of you is a sight that\nfills you with dread, 5 feet and 3 inches of the pastry queen herself. She stairs\nyou dead in the eye, points are you with her bony finger and an evil, wry smile\nspreads across her face.",
 
     "exits": {"south": "room_puzzle_maze"},
 
     "event": "boss",
 
-    "items": [item_flour]
+    "items": ["***Super Secret Formula ITEM/WINNING***"]
 }
 
-room_boss_2 = {
-    "id": "room_boss_2",
+room_boss_paul = {
+    "id": "room_boss_paul",
 
     "name": "boss_2",
 
-    "story": "*** description ***",
+    "story": "You enter the walk in freezer. Immediately you are chilled to your core, but not\nbecause of the temperature, but instead because of what stands in front of you.\nA zombie like Paul Hollywood stands with his arms crossed, staring coldly at you.\nHis eyes black and cavernous. His heart has been turned to ice Mary Berry and now\nhis only sanctuary is in the confines of the  walk in freezer. You notice in his\nback pocket that there's a key made from ginger bread.",
 
     "exits": {},
 
     "event": "boss",
 
-    "items": [item_butter]
+    "items": ["***Ginger KEY ITEM***"]
 }
 
-room_boss_3 = {
-    "id": "room_boss_3",
+room_boss_ms = {
+    "id": "room_boss_ms",
 
     "name": "boss_3",
 
-    "story": "*** description ***",
+    "story": "You enter the store room. As you walk down a narrow hallway you notice that it\n becomes continually larger with each step you take. You can hear a mumbling of \nvoices which get louder and clearer as you approach the room. There you find Mel \nand Sue cracking jokes full of innuendo, some so bad that it pains your ears. It \ntakes them a moment to notice you but they then fall silent as they turn to face \nyou, their eyes piercing like daggers in your chest. You notice a key made of \nshort bread in Mel’s jacket pocket. ",
 
     "exits": {},
 
     "event": "boss",
 
-    "items": [item_milk]
+    "items": ["***Shortbread KEY ITEM***"]
 }
 
-room_boss_4 = {
-    "id": "room_boss_4",
-
-    "name": "boss_4",
-
-    "story": "*** description ***",
-
-    "exits": {},
-
-    "event": "boss",
-
-    "items": [item_eggs]
-}
 
 
 rooms = {
     "room_centre": room_centre,
+    "room_puzzle_keypad": room_puzzle_keypad,
+    "room_puzzle_crossword": room_puzzle_crossword,
     "room_puzzle_maze": room_puzzle_maze,
     "room_boss_mary": room_boss_mary,
-    "room_boss_2": room_boss_2,
-    "room_boss_3": room_boss_3,
-    "room_boss_4": room_boss_4
+    "room_boss_paul": room_boss_paul,
+    "room_boss_ms": room_boss_ms
 }
 
-puzzle_rooms = [room_puzzle_2, room_puzzle_3, room_puzzle_4, room_puzzle_5, room_puzzle_trivia]
+puzzle_rooms = [room_puzzle_keypad, room_puzzle_crossword, room_puzzle_maze]
