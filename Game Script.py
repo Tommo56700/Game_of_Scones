9ce4098687a8139713c8a0c4f05473683c9a5c30
DanHow96 <daniel.howorth@sky.com>
2016-10-17 15:33:02 +0100

    Add files via upload

---
+++ Game Script.py
@@ -0,0 +1,107 @@
+"""
+Welcome! 
+
+In a land where baking is a skill revered among gods and men alike,
+one humble young baker strives to conquer all by defeating the tyrannical queen
+of baking herself, Mary Berry, and delivering fortune and glory to House Starch.
+For too long has this evil overlord of the croissant reigned down on all other
+noble patissiers. Cursed by an evil spell placed on them, all baking endeavours
+attempted by anyone other than Mary Berry will burn, fall flat or just fail
+miserably. It is time that they rise up and break the dark mistress’s monopoly
+on all things baked by stealing the secret recipe for the source of her power,
+The Scone of Enchantment. This perilous quest is a task left to you, master baker.
+Good luck! 
+
+Starting room –  
+
+You have infiltrated the lair of the queen, but be careful not to wake the beast.
+The journey here so far has been a long and challenging one, but things are
+about to get harder. You find yourself standing in a vast, expansive room in
+the centre of the Red Velvet Keep, the palace from which Mary Berry pulls the
+strings over the Seven Kingdoms. The room is airily chilling.
+The air is filled with flour dust, illuminated by strands of light beaming in
+from the windows. The walls are adorned with paintings of baked goods and in
+the centre is a stone statue of a croquembouche.  On the north wall is a large,
+arched doorway with two key holes. One appears to be made from ginger bread,
+the other from shortbread. To the west is a door labelled Pantry. To the east
+is a door labelled Cellar, and a stairway leading down.  
+
+Pantry 
+
+You are in the pantry. Surrounding you from all directions are shelves towering
+all the way to the ceiling, stacked full of pastries, cakes, loafs of bread and
+a plethora of cooking ingredients. You notice a chest with the lid slightly
+ajar. On the west wall is a door Walk in freezer.  In the centre of the room
+(insert puzzle).  
+
+(Chest – Open chest – In the chest you find a large wooden rolling pin.
+The rolling pin looks quite heavy and would certainly do a lot of damage if
+bludgeoned relentlessly against someone’s skull.) 
+
+Walk in freezer 
+
+You enter the walk in freezer. Immediately you are chilled to your core, but not
+because of the temperature, but instead because of what stands in front of you.
+A zombie like Paul Hollywood stands with his arms crossed, staring coldly at you.
+His eyes black and cavernous. His heart has been turned to ice Mary Berry and now
+his only sanctuary is in the confines of the  walk in freezer. You notice in his
+back pocket a key made from ginger bread.  
+
+(Battle Paul Hollywood – use rolling pin – take key) 
+
+Cellar 
+
+You descend into the under belly of Mary’s fortress of pain. The cellar is dark
+and damp, the only light coming from a few flickering candles hung on the walls.
+Empty crates and wooden pallets scatter the room. You notice on the floor an
+elaborate rug, an heirloom of the Berry family no doubt. One of the corners is
+upturned slightly. On the east wall is a door labelled store room.  In the centre
+of the room is (insert puzzle).  
+
+(Move rug – There is a trap door underneath – open trap door – you have
+discovered a cubby hole. In it you find (insert weapon) (a bag of salt,
+sure to inflict a lot of pain were it accidentally thrown into the eyes of an
+unsuspecting by stander). 
+
+Store room 
+
+You enter the store room. You walk down a narrow hallway where at the end the
+room opens up. You can hear a mumbling of voices which get louder and clearer as
+you approach the room. There you find Mel and Sue cracking jokes full of
+innuendo, some so bad that it pains your ears. It takes them a moment to notice
+you but they then fall silent as they turn to face you, their eyes piercing like
+daggers in your chest. You notice a key made of short bread in Mel’s jacket
+pocket. 
+
+(Fight Mel and Sue – take key – return to main room) 
+
+You carefully slot the keys into their respective key holes and turn them
+simultaneously. You hear a bolt crank back and the creaking door slides open,
+revealing what appears to be a maze. (insert maze description) 
+
+Main baking you 
+
+You have made it this far but you must now rely on your baking ability more
+than ever. This is it. The final showdown. The stage is set, the show must now
+unfold. And the far end of the room is a great fiery furnace, the heat from
+which is scolding even from this distance. You have well and truly descended
+into Mary’s hell. In front of the furnace you see four ingredients laid out on a
+table; mill, flour, butter and eggs. But there in front of you is a sight that
+fills you with dread, 5 feet and 3 inches of the pastry queen herself. Stairs
+you dead in the eye, points are you with her bony finger and an evil, wry smile
+spreads across her face. (fight Mary Berry)  
+
+Ding dong old chap, the wicked witch is dead! You pry a piece of crumpled
+parchment from her cold, dead hands which reveals to you the secret recipe of
+Mary’s magical scones. (Flour, butter, milk, eggs). All you have to do now is
+bake the perfect scone and the curse that has plagued this land will be forever
+broken.  
+
+(Scone baked in right order – bakes perfect scone – and there it is, a delicious,
+light, fluffy scone. Bards will marvel over your greatness for years to come and
+House Starch will be immortalised forever in song. Well done!) 
+
+Scone baked in wrong order or Mary defeats you -  oh dear, it appears you have
+come all this way just to become unstuck by a soggy bottom. You have brought
+shame upon House Starch!) 
+"""
