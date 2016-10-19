cf095fc0002ef21454f475e246123236962e33a4
SparkeyRob <rdstaylor2@gmail.com>
2016-10-19 11:34:46 +0100

    Winning and Losing functions

---
+++ game over.py
@@ -0,0 +1,248 @@
+import time
+
+def clear():
+    print(' \n' * 75)
+
+def game_over():
+  clear()
+  print('''
+    .oooooo.                                          
+
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+    .oooooo.                                          
+   d8P'  `Y8b                                         
+   
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+    .oooooo.                                          
+   d8P'  `Y8b                                         
+  888            .oooo.   ooo. .oo.  .oo.    .ooooo.  
+
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+    .oooooo.                                          
+   d8P'  `Y8b                                         
+  888            .oooo.   ooo. .oo.  .oo.    .ooooo.  
+  888           `P  )88b  `888P"Y88bP"Y88b  d88' `88b 
+   
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+    .oooooo.                                          
+   d8P'  `Y8b                                         
+  888            .oooo.   ooo. .oo.  .oo.    .ooooo.  
+  888           `P  )88b  `888P"Y88bP"Y88b  d88' `88b 
+  888     ooooo  .oP"888   888   888   888  888ooo888 
+    
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+    .oooooo.                                          
+   d8P'  `Y8b                                         
+  888            .oooo.   ooo. .oo.  .oo.    .ooooo.  
+  888           `P  )88b  `888P"Y88bP"Y88b  d88' `88b 
+  888     ooooo  .oP"888   888   888   888  888ooo888 
+  `88.    .88'  d8(  888   888   888   888  888    .o 
+   
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+    .oooooo.                                          
+   d8P'  `Y8b                                         
+  888            .oooo.   ooo. .oo.  .oo.    .ooooo.  
+  888           `P  )88b  `888P"Y88bP"Y88b  d88' `88b 
+  888     ooooo  .oP"888   888   888   888  888ooo888 
+  `88.    .88'  d8(  888   888   888   888  888    .o 
+   `Y8bood8P'   `Y888""8o o888o o888o o888o `Y8bod8P' 
+   
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+    .oooooo.                                          
+   d8P'  `Y8b                                         
+  888            .oooo.   ooo. .oo.  .oo.    .ooooo.  
+  888           `P  )88b  `888P"Y88bP"Y88b  d88' `88b 
+  888     ooooo  .oP"888   888   888   888  888ooo888 
+  `88.    .88'  d8(  888   888   888   888  888    .o 
+   `Y8bood8P'   `Y888""8o o888o o888o o888o `Y8bod8P' 
+                                                      
+                                                      
+                                                      
+    .oooooo.                                  
+    
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+    .oooooo.                                          
+   d8P'  `Y8b                                         
+  888            .oooo.   ooo. .oo.  .oo.    .ooooo.  
+  888           `P  )88b  `888P"Y88bP"Y88b  d88' `88b 
+  888     ooooo  .oP"888   888   888   888  888ooo888 
+  `88.    .88'  d8(  888   888   888   888  888    .o 
+   `Y8bood8P'   `Y888""8o o888o o888o o888o `Y8bod8P' 
+                                                      
+                                                      
+                                                      
+    .oooooo.                                  
+   d8P'  `Y8b                                 
+   
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+    .oooooo.                                          
+   d8P'  `Y8b                                         
+  888            .oooo.   ooo. .oo.  .oo.    .ooooo.  
+  888           `P  )88b  `888P"Y88bP"Y88b  d88' `88b 
+  888     ooooo  .oP"888   888   888   888  888ooo888 
+  `88.    .88'  d8(  888   888   888   888  888    .o 
+   `Y8bood8P'   `Y888""8o o888o o888o o888o `Y8bod8P' 
+                                                      
+                                                      
+                                                      
+    .oooooo.                                  
+   d8P'  `Y8b                                 
+  888      888 oooo    ooo  .ooooo.  oooo d8b 
+    
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+    .oooooo.                                          
+   d8P'  `Y8b                                         
+  888            .oooo.   ooo. .oo.  .oo.    .ooooo.  
+  888           `P  )88b  `888P"Y88bP"Y88b  d88' `88b 
+  888     ooooo  .oP"888   888   888   888  888ooo888 
+  `88.    .88'  d8(  888   888   888   888  888    .o 
+   `Y8bood8P'   `Y888""8o o888o o888o o888o `Y8bod8P' 
+                                                      
+                                                      
+                                                      
+    .oooooo.                                  
+   d8P'  `Y8b                                 
+  888      888 oooo    ooo  .ooooo.  oooo d8b 
+  888      888  `88.  .8'  d88' `88b `888""8P 
+    
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+    .oooooo.                                          
+   d8P'  `Y8b                                         
+  888            .oooo.   ooo. .oo.  .oo.    .ooooo.  
+  888           `P  )88b  `888P"Y88bP"Y88b  d88' `88b 
+  888     ooooo  .oP"888   888   888   888  888ooo888 
+  `88.    .88'  d8(  888   888   888   888  888    .o 
+   `Y8bood8P'   `Y888""8o o888o o888o o888o `Y8bod8P' 
+                                                      
+                                                      
+                                                      
+    .oooooo.                                  
+   d8P'  `Y8b                                 
+  888      888 oooo    ooo  .ooooo.  oooo d8b 
+  888      888  `88.  .8'  d88' `88b `888""8P 
+  888      888   `88..8'   888ooo888  888     
+   
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+    .oooooo.                                          
+   d8P'  `Y8b                                         
+  888            .oooo.   ooo. .oo.  .oo.    .ooooo.  
+  888           `P  )88b  `888P"Y88bP"Y88b  d88' `88b 
+  888     ooooo  .oP"888   888   888   888  888ooo888 
+  `88.    .88'  d8(  888   888   888   888  888    .o 
+   `Y8bood8P'   `Y888""8o o888o o888o o888o `Y8bod8P' 
+                                                      
+                                                      
+                                                      
+    .oooooo.                                  
+   d8P'  `Y8b                                 
+  888      888 oooo    ooo  .ooooo.  oooo d8b 
+  888      888  `88.  .8'  d88' `88b `888""8P 
+  888      888   `88..8'   888ooo888  888     
+  `88b    d88'    `888'    888    .o  888     
+   
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+    .oooooo.                                          
+   d8P'  `Y8b                                         
+  888            .oooo.   ooo. .oo.  .oo.    .ooooo.  
+  888           `P  )88b  `888P"Y88bP"Y88b  d88' `88b 
+  888     ooooo  .oP"888   888   888   888  888ooo888 
+  `88.    .88'  d8(  888   888   888   888  888    .o 
+   `Y8bood8P'   `Y888""8o o888o o888o o888o `Y8bod8P' 
+                                                      
+                                                      
+                                                      
+    .oooooo.                                  
+   d8P'  `Y8b                                 
+  888      888 oooo    ooo  .ooooo.  oooo d8b 
+  888      888  `88.  .8'  d88' `88b `888""8P 
+  888      888   `88..8'   888ooo888  888     
+  `88b    d88'    `888'    888    .o  888     
+   `Y8bood8P'      `8'     `Y8bod8P' d888b  
+   ''')
+  time.sleep(5)
+  clear()
+  time.sleep(2)
+  print('''
+  	       -/oydmmNNNNNmdyo/-                
+               :ymNNNNNNNNNNNNNNNNNNNy/`            
+            `+mNNNNNNNNNNNNNNNNNNNNNNNNmo`          
+           -dNNNNNNNNNNNNNNNNNNNNNNNNNNNNd-         
+          -mNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN-        
+          dNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNd        
+         -NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN:       
+         /NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN/       
+         :NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN:       
+         .NNNNNNms///+yNNNNNNNNdo/:/ohNNNNNN`       
+          hNNNNm.      /NNNNNNh`     `yNNNNo        
+          .dNNNy       .NNNNNN/       /NNNh`        
+           .dNNd       /NNNNNNs       sNNd.         
+            .mNNy:.`.-+mNNmmNNNo-.`.-sNNm.          
+             oNNNNmmmmNNNh-.yNNNNmmmNNNNs           
+             -NNNNNNNNNNo`  `/NNNNNNNNNN:           
+              :shmNNNNNN+-::-/NNNNNNmhs/            
+                `.:ohNNNNNNNNNNNNho:.`              
+                 -y+ sNNNNNNNNNNy`oy.               
+                 `NN+`-/++++++/:`oNd                
+                  oNNh/-......-/hNN/                
+                  `:hNNNmmmmmNNNmy:                 
+    .::.`            -dNNNNNNNNh-             `--`  
+   `mNNNh/`           `:shddhs:`            -odNNd` 
+   .NNNNNNd+`                             -yNNNNNN` 
+   `mNNNNNNNd/.`                      `.:yNNNNNNNm` 
+  `yNNNNNNNNNNNdyo:.`             `-/shmNNNNNNNNNNh.
+  hNNNNNdydmmNNNNNNNhs/.`    ``:oymNNNNNNmmhydNNNNNh
+  :yhyo-   `.:+sdmNNNNNNds/:ohmNNNNNmds+:.   `-ohdd+
+                 .:+hmNNNNNNNNNNmho:.               
+                   `-sNNNNNNNNNNs-`                 
+                `/smNNNNNmyhmNNNNNds:`              
+             `/yNNNNNNmo:`  `/ymNNNNNmy/`           
+      ``.-/+yNNNNNNms:         `:ymNNNNNNy+-.```    
+  `/yhmNNNNNNNNNNh/`              `/hNNNNNNNNNNdyo- 
+  dNNNNNNNNNNNNs.                    -sNNNNNNNNNNNNy
+  yNNNNNNNNNNN.                        -NNNNNNNNNNNy
+   `://yNNNNNy                          +NNNNNms+:` 
+        :hNms`                           /dNd+`     
+
+  	''')
+  time.sleep(5)
+game_over()
+input()
---
+++ you won.py
@@ -0,0 +1,243 @@
+import time
+
+def clear():
+    print(' \n' * 75)
+def you_won():
+  clear()
+  print('''
+  oooooo   oooo                       
+
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+  oooooo   oooo                       
+   `888.   .8'                        
+
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+  oooooo   oooo                       
+   `888.   .8'                        
+    `888. .8'    .ooooo.  oooo  oooo  
+
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+  oooooo   oooo                       
+   `888.   .8'                        
+    `888. .8'    .ooooo.  oooo  oooo  
+     `888.8'    d88' `88b `888  `888  
+
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+  oooooo   oooo                       
+   `888.   .8'                        
+    `888. .8'    .ooooo.  oooo  oooo  
+     `888.8'    d88' `88b `888  `888  
+      `888'     888   888  888   888  
+
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+  oooooo   oooo                       
+   `888.   .8'                        
+    `888. .8'    .ooooo.  oooo  oooo  
+     `888.8'    d88' `88b `888  `888  
+      `888'     888   888  888   888  
+       888      888   888  888   888  
+
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+  oooooo   oooo                       
+   `888.   .8'                        
+    `888. .8'    .ooooo.  oooo  oooo  
+     `888.8'    d88' `88b `888  `888  
+      `888'     888   888  888   888  
+       888      888   888  888   888  
+      o888o     `Y8bod8P'  `V88V"V8P' 
+
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+  oooooo   oooo                       
+   `888.   .8'                        
+    `888. .8'    .ooooo.  oooo  oooo  
+     `888.8'    d88' `88b `888  `888  
+      `888'     888   888  888   888  
+       888      888   888  888   888  
+      o888o     `Y8bod8P'  `V88V"V8P' 
+                                      
+                                      
+                                      
+  oooooo   oooooo     oooo                       
+
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+  oooooo   oooo                       
+   `888.   .8'                        
+    `888. .8'    .ooooo.  oooo  oooo  
+     `888.8'    d88' `88b `888  `888  
+      `888'     888   888  888   888  
+       888      888   888  888   888  
+      o888o     `Y8bod8P'  `V88V"V8P' 
+                                      
+                                      
+                                      
+  oooooo   oooooo     oooo                       
+   `888.    `888.     .8'                        
+
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+  oooooo   oooo                       
+   `888.   .8'                        
+    `888. .8'    .ooooo.  oooo  oooo  
+     `888.8'    d88' `88b `888  `888  
+      `888'     888   888  888   888  
+       888      888   888  888   888  
+      o888o     `Y8bod8P'  `V88V"V8P' 
+                                      
+                                      
+                                      
+  oooooo   oooooo     oooo                       
+   `888.    `888.     .8'                        
+    `888.   .8888.   .8'    .ooooo.  ooo. .oo.   
+
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+  oooooo   oooo                       
+   `888.   .8'                        
+    `888. .8'    .ooooo.  oooo  oooo  
+     `888.8'    d88' `88b `888  `888  
+      `888'     888   888  888   888  
+       888      888   888  888   888  
+      o888o     `Y8bod8P'  `V88V"V8P' 
+                                      
+                                      
+                                      
+  oooooo   oooooo     oooo                       
+   `888.    `888.     .8'                        
+    `888.   .8888.   .8'    .ooooo.  ooo. .oo.   
+     `888  .8'`888. .8'    d88' `88b `888P"Y88b  
+
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+  oooooo   oooo                       
+   `888.   .8'                        
+    `888. .8'    .ooooo.  oooo  oooo  
+     `888.8'    d88' `88b `888  `888  
+      `888'     888   888  888   888  
+       888      888   888  888   888  
+      o888o     `Y8bod8P'  `V88V"V8P' 
+                                      
+                                      
+                                      
+  oooooo   oooooo     oooo                       
+   `888.    `888.     .8'                        
+    `888.   .8888.   .8'    .ooooo.  ooo. .oo.   
+     `888  .8'`888. .8'    d88' `88b `888P"Y88b  
+      `888.8'  `888.8'     888   888  888   888  
+
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+  oooooo   oooo                       
+   `888.   .8'                        
+    `888. .8'    .ooooo.  oooo  oooo  
+     `888.8'    d88' `88b `888  `888  
+      `888'     888   888  888   888  
+       888      888   888  888   888  
+      o888o     `Y8bod8P'  `V88V"V8P' 
+                                      
+                                      
+                                      
+  oooooo   oooooo     oooo                       
+   `888.    `888.     .8'                        
+    `888.   .8888.   .8'    .ooooo.  ooo. .oo.   
+     `888  .8'`888. .8'    d88' `88b `888P"Y88b  
+      `888.8'  `888.8'     888   888  888   888  
+       `888'    `888'      888   888  888   888  
+
+   ''')
+  time.sleep(0.3)
+  clear()
+  print('''
+  oooooo   oooo                       
+   `888.   .8'                        
+    `888. .8'    .ooooo.  oooo  oooo  
+     `888.8'    d88' `88b `888  `888  
+      `888'     888   888  888   888  
+       888      888   888  888   888  
+      o888o     `Y8bod8P'  `V88V"V8P' 
+                                      
+                                      
+                                      
+  oooooo   oooooo     oooo                       
+   `888.    `888.     .8'                        
+    `888.   .8888.   .8'    .ooooo.  ooo. .oo.   
+     `888  .8'`888. .8'    d88' `88b `888P"Y88b  
+      `888.8'  `888.8'     888   888  888   888  
+       `888'    `888'      888   888  888   888  
+        `8'      `8'       `Y8bod8P' o888o o888o
+   ''')
+  time.sleep(5)
+  clear()
+  time.sleep(1)
+  print('''
+                                                                        
+                                                                        
+                                                                        
+                                                                        
+                               ...... . .                               
+                          ,,iiirir7L77rr7r:i:,                          
+                      .ii:7rr:..,     . ,.::7i;r:.                      
+                    .;rii:                   .,ri77,                    
+                 .::,:i.                       ..,i:7.                  
+                ,rii,,         ,                  ..:i:                 
+              .:i:.,,.    .   .,, ,        . . . . ...,i.               
+             .::......   .. ,,., , . . . .  . .,.,. :i.,ii              
+            .::.......:, :,.....,., , . . .. .:.,.,..,,..i:             
+            i,.....,..    .,.......,.. ....,., . .........i,            
+           ,:.......          ......... . ..        .,.....r.           
+           r.......  7EGB0X.  ............   .LPr,  .,....:r:           
+          ,i,,..... MBBBBBBB1  .........,  rBBBBBBB. ......,L           
+          r,,..... BB,     7Br .........  LBB:  .FBBi.......i,          
+         .;i......           ...,........ Y:       LL .....,i:          
+          i,...,.... ..:.:.. ....,........     .     ..,..,.:i          
+         .;::...   .,.,.,.,., .......,.... .,.,,:.,.  ....:.::          
+          i,...  ,ir.,.,.,.,.,.,.,......,.,........    .,,.::i          
+          i;:...BBB. .,.:.,.,.,.,.............,.,. UBB7 ..,.;:          
+          .i,, ,S5BF   ,.,.,.,......,.,.,......    BBqE:...::           
+           ii.:   :BBi   .,.,...,..,.,.,......   ;BE.  .  ,r:           
+           .i:,i.  .1BBr     ..,.,,.....,.    ,1BB7  . ..,;i            
+            ,r,.,:,  ,5BB1r                ;7NBBu.  . ,:,ir             
+             :7:,,:i   .iMBBBFYJ:. : :r7u0BBBN:.  .:.,:,:i.             
+              ,:ii.::,    ,iUBBBBBBBBBBBBML,.   ..,i,:.;r.              
+               ,:ri....:.      . ......       .,....iii.                
+                 .irii,:,...              ..,:,,,:::;:                  
+                   .:ii.i::..:.,,,.,.,..:i..::.,iiJ;.                   
+                     .iri:;::i:.:,i:,,i:i.i::,iYr:                      
+                        .,i7iiirir,i:r.;:i:ii:,                         
+                            ..,:::r:;irii.,                             
+                                                                        
+  ''')
+  time.sleep(5)
+you_won()
+input()
