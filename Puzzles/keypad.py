5f40eec39f55cb5fc55a6e9df981db6ce057e663
KieranJames1996 <kieranparnell2013@live.co.uk>
2016-10-19 11:02:04 +0100

    Keypad (Editied to fit stroy)

--- Puzzles/keypad.py
+++ Puzzles/keypad.py
@@ -1,8 +1,8 @@
 def puzzle_keypad():
 	import random 
 	number = random.randrange (0,9) #random correct number in the range of 0-9 
-	health = 100 #edit this to match game health variable name
-	print ("You've encountered a keypad,")# edit this to cahnge introudction of puzzle 
+	health = 100 #edit this to match ga,")#me health variable name
+	print ("You've encountered a keypad which which is connected to a door") #edit this to cahnge introudction of puzzle 
 	print ("You've already guessed the first three numbers from the keypad as flour was still on the keys")
 	print ("Now you must now guess the last digit")
 
@@ -18,8 +18,8 @@ def puzzle_keypad():
 			health = health - 10
 			print ("You have been shocked by the keypad. You lost 10 health, and you have ",  health, "remaining")
 			temp= int(input("Guess again: "))
-	print("You have correctly guessed the numberpad code") #Discription for completing the puzzle 
+	print("You have correctly guessed the numberpad code") #Discription for completing the puzzle
 	return health
+	print("Finally the door oepns, you step through and enter the next room that awaits") 
 #Text validation still needs to be implemented into the puzzle, for example, only allowing digits
 # runs the above function
-puzzle_keypad()
